5676029356753a57325372f46b89fc534748f53c
ebio80 <115435133+ebio80@users.noreply.github.com>
2024-03-02 22:52:49 -0600

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ekajane-terraform" # Replace with your actual S3 bucket name
+    bucket = "aqueno-1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
